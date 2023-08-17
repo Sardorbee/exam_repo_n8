7d7f71a0fa0d82b8ac0e7f42dd5426d93edb87fb
Sardorbee <sardorgoyibnazarov@gmail.com>
2023-08-17 18:31:03 +0500

    first commit from sardor

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,19 @@
-// Enter Name::  
-// Baxolash:
-// 1. Dio task: 20 ball.
-// 2. Firebase task : 20 ball.
-// 3. Map task : 20 ball.
-// 4. Custom Paint and Animations: 20 ball.
-// 5. Suxbat: 20 ball
+
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("hello")),
+      ),
+    );
+  }
+}
